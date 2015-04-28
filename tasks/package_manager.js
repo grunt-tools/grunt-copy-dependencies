0e0448c0c58a602172325e8788bbe922cce53303
Jesús Germade <jesus@germade.es>
2015-04-28 08:14:57 +0200

    first implementation

--- tasks/package_manager.js
+++ tasks/package_manager.js
@@ -14,37 +14,32 @@ module.exports = function(grunt) {
   // creation: http://gruntjs.com/creating-tasks
 
   grunt.registerMultiTask('package_manager', 'Package Manager for dependencies', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
+
     var options = this.options({
-      punctuation: '.',
-      separator: ', '
+          manager: 'bower'
+        }),
+        path = require('path'),
+        manager = require('package-manager'),
+        cwd = process.cwd(),
+        src = [''];
+
+    if( !options.dest || path.join( cwd, options.dest ) === cwd ) {
+      throw 'invalid dest dir: ' + options.dest;
+    }
+
+    if( typeof options.src === 'string' ) {
+      src = [options.src];
+    } else if( options.src instanceof Array ) {
+      src = options.src;
+    } else if( options.src ) {
+      throw 'src should be a string or array of strings';
+    }
+
+    src.forEach(function (iSrc) {
+      manager.find({ cwd: options.cwd, src: iSrc || 'dependencis', append: true });
     });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
+    manager.copy(dest);
   });
 
 };
